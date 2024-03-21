3075f3df5182896712614fdbf7ab8b0e38628138
rafael <devrafaelalvarenga@gmail.com>
2024-03-20 22:17:04 -0300

    update desafio.py

--- desafio.py
+++ desafio.py
@@ -1,34 +1,43 @@
 #Calculo bonus KPI de 2024
 
+import sys
+
 ano_calculo = 2024
-constante_bonus = 1000
+base_calculo_bonus = 1000
 
 #Solicite ao usuario que digite seu nome
-nome_usuario = input('Digite seu nome: ').strip()
-if nome_usuario.isdigit():
-    print('Verifique o nome digitado')
-    exit()
-elif len(nome_usuario) == 0:
-    print('Verifique o nome digitado')
-    exit()   
-elif nome_usuario.isspace():
-    print('Verifique o nome digitado')
-    exit()        
+try:
+    nome_usuario = input('Digite seu nome: ')
+
+    #Verifica se nome contem digitos
+    if any(char.isdigit() for char in nome_usuario):
+        sys.exit('O nome não deve conter numeros.')
+    #Verifica se nome esta vazio    
+    elif len(nome_usuario) == 0:
+        sys.exit('O nome não pode estar vazio.')
+    else:
+        print('Nome valido')
+except ValueError as e:
+    sys.exit(e)                
 
 #Solicite ao usuario que digite o valor do seu salario
-valor_salario = float(input('Digite o valor do seu salario: '))
-if valor_salario < 0:
-    print('Verifique o valor digitado')
-    exit()
+try:    
+    valor_salario = float(input('Digite o valor do seu salario: '))
+    if valor_salario < 0:
+        sys.exit('Verifique o valor digitado. Calculo não realizado com valor negativo.')
+except ValueError:
+    sys.exit('Verifique o valor digitado')
 
 #Solicite ao usuario que digite o valor do bonus recebido (Quantidade salario)
-valor_bonus = float(input('Digite o valor do bonus recebido: '))
-if valor_bonus < 0:
-    print('Verifique o valor digitado')
-    exit()
+try:    
+    valor_bonus = float(input('Digite o valor do bonus recebido: '))
+    if valor_bonus < 0:
+        sys.exit('Verifique o valor digitado. Calculo não realizado com valor negativo.')
+except ValueError:
+    sys.exit('Verifique o valor digitado')
 
 #Calcule o valor do bonus final '1000 + salario * bonus'
-valor_bonus_final = constante_bonus + (valor_salario * valor_bonus)
+valor_bonus_final = base_calculo_bonus + (valor_salario * valor_bonus)
 
 #Imprima mensagem personalizada incluindo nome do usuario e valor do bonus
-print(f'{nome_usuario.capitalize()} seu bonus em {ano_calculo} é de R${valor_bonus_final}')
+print(f'{nome_usuario.capitalize()} seu bonus em {ano_calculo} é de R${valor_bonus_final:.2f}')
